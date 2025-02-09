02d563ba16f01520a0fb68f346e2060da282978f
Prathiksha.m <150207438+PradhikshaLM@users.noreply.github.com>
2025-02-09 20:17:13 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,14 +1,14 @@
-// JavaScript source code
+
 function Message_sent() {
     var n = document.getElementById("n1").value;
     var e = document.getElementById("e1").value;
     var s = document.getElementById("s1").value;
     var m = document.getElementById("m1").value;
-    //Code for validation  
+     
     var letters = /^[A-Za-z]+$/;
     var email_val = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     
-    //other validations required code  
+  
     if (n == '' || e == '' || s == '' || m == '') {
         alert("Enter each details correctly");
     }
@@ -26,7 +26,7 @@ function Message_sent() {
     }
     else {
         alert("Your message has been sent successfully... Redirecting to homepage");
-        window.location = "Home.html";
+        window.location = "index.html";
     }
 }  
 
